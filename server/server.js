f94700ed42177e869b57754efb474bfcdc8311cf
Muhammad Alam <malam53345@gmail.com>
2023-12-10 17:27:06 +0500

    updated

--- server/server.js
+++ server/server.js
@@ -5,7 +5,7 @@ require("./db/conn");
 const userRouter = require("./routes/userRoutes");
 const doctorRouter = require("./routes/doctorRoutes");
 const appointRouter = require("./routes/appointRoutes");
-const path = require("path");
+// const path = require("path");
 const notificationRouter = require("./routes/notificationRouter");
 
 const app = express();
@@ -17,11 +17,11 @@ app.use("/api/user", userRouter);
 app.use("/api/doctor", doctorRouter);
 app.use("/api/appointment", appointRouter);
 app.use("/api/notification", notificationRouter);
-app.use(express.static(path.join(__dirname, "./client/build")));
+// app.use(express.static(path.join(__dirname, "./client/build")));
 
-app.get("*", (req, res) => {
-  res.sendFile(path.join(__dirname, "../client/build/index.html"));
-});
+// app.get("*", (req, res) => {
+//   res.sendFile(path.join(__dirname, "../client/build/index.html"));
+// });
 
 app.listen(port, () => {
   console.log("App is listening on port " + port);
